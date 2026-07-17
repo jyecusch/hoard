b22a0d957aeb7d83e50a33c65d5065b6a93dc38b
Jye Cusch <jye.cusch@gmail.com>
2026-07-18 08:14:27 +1000

    remove old prototype

--- deploy/init-db.sql
+++
@@ -1,3 +0,0 @@
--- Create additional databases required by Zero
-CREATE DATABASE hoard_cvr;
-CREATE DATABASE hoard_cdb;
--- src/lib/env.ts
+++
@@ -1,4 +0,0 @@
-export const env = {
-    DATABASE_URL: process.env.DATABASE_URL!,
-    ZERO_UPSTREAM_DB: process.env.ZERO_UPSTREAM_DB!
-};
